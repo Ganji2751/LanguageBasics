5a5ee71050a08e21c793b29b27a78c713432321f
Ganji2751 <58478050+Ganji2751@users.noreply.github.com>
2019-12-03 21:15:37 +0530

    Add files via upload

---
+++ P2.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.*;
+
+class P2
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome "+args[0]);
+	}
+}
---
+++ P3.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+import java.util.*;
+
+class P3
+{
+	public static void main(String number[])
+	{
+		int a = Integer.parseInt(number[0]);
+		int b = Integer.parseInt(number[1]);
+		System.out.println("The Sum of "+a+" and "+b+" is "+(a+b));
+	}
+}
